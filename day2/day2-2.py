9f495de2a585fc19561cf54d79ac964bbe9aa99f
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2019-12-17 08:41:59 +0000

    Day2 - Complete

--- day2/day2-2.py
+++ day2/day2-2.py
@@ -1,10 +1,10 @@
 f = open("./input.txt", "r")
 data = list(map(int, f.read().split(",")))
-data[1] = 12
-data[2] = 2
+data[1] = 0
+data[2] = 0
 
 
-def main():
+def getOutput(data):
     for x in range(0, len(data), 4):
         if data[x] == 1:
             data[data[x+3]] = data[data[x + 2]] + \
@@ -13,8 +13,31 @@ def main():
             data[data[x+3]] = data[data[x + 2]] * \
                 data[data[x + 1]]
         elif data[x] == 99:
-            print(data)
-            exit()
+            return data[0]
 
 
-main()
+def main(target):
+    noun = 0
+    verb = 0
+    while noun <= 99:
+        val = getOutput(data.copy())
+        if val == target:
+            break
+        elif val < target:
+            noun = noun + 1
+            data[1] = noun
+        elif val > target:
+            noun = noun - 1
+            data[1] = noun
+            break
+    while verb <= 99:
+        val = getOutput(data.copy())
+        if val == target:
+            break
+        elif val < target:
+            verb = verb + 1
+            data[2] = verb
+    print(100 * noun + verb)
+
+
+main(19690720)
