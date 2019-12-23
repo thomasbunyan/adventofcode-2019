4681b273ee1166026c4341d0cc3761463041ff2c
Thomas Bunyan <thomaspaulbunyan@gmail.com>
2019-12-23 01:17:50 +0000

    Day14 part 1 - Complete

--- day14/day14.py
+++ day14/day14.py
@@ -21,56 +21,27 @@ fuel_requirements = conversions["FUEL"]
 
 def calc(n, item, waste):
     if item == "ORE":
-        return n
+        return (n, waste)
     else:
-        ans = 0
+        if not item in waste:
+            waste[item] = 0
         (nget, need) = conversions[item]
-        print("Need:", n, "of", item)
+        while n > 0 and waste[item] > 0:
+            n -= 1
+            waste[item] -= 1
         batches = (math.ceil(n/nget))
         made = batches * nget
-        print(item, batches, "reaction will produce:", made)
-        print("Leftovers:", made-n, "\n")
+        leftovers = made-n
+        waste[item] += leftovers
+        # print("Need:", n, "of", item)
+        # print(item, batches, "reaction will produce:", made)
+        # print("Leftovers:", leftovers, "\n")
+        ans = 0
         for (ny, y) in need:
-            ans += batches*calc(int(y), ny, waste)
-        return ans
+            (val, w) = calc(int(y) * batches, ny, waste)
+            ans += val
+            waste = w
+        return (ans, waste)
 
 
-print(calc(1, "FUEL"), dict())
-
-
-# def calc(req, required, waste2):
-#     inputs = req["input"]
-#     if inputs[0][0] == "ORE":
-#         # print(required)
-#         if not required[0] in waste2:
-#             waste2[required[0]] = 0
-#         ores = waste2[required[0]]
-#         ore_val = int(req["input"][0][1])
-#         i = 0
-#         while ores < required[1]:
-#             ores += int(req["out_amount"])
-#             i += 1
-#         waste2[required[0]] = ores - required[1]
-#         if not "ans" in waste2:
-#             waste2["ans"] = 0
-#         waste2["ans"] += i * ore_val
-#         # print(i * ore_val)
-#         # print(waste2)
-#         return waste2
-
-#     for i in inputs:
-#         element = i[0]
-#         amount = int(req["out_amount"])
-#         e_yield = int(i[1]) * amount
-#         # print(element, e_yield, amount, e_yield * amount, required[1])
-#         # e_yield *= required[1]
-#         print(e_yield, required[1])
-#         counter = 0
-#         while e_yield < required[1]:
-#             e_yield += (int(i[1]) * amount)
-#             counter += 1
-#         print(e_yield, required[1])
-
-#         waste2 = calc(conversions[[i][0][0]], (element,  e_yield), waste2)
-
-#     return waste2
+print(calc(1, "FUEL", dict()))
